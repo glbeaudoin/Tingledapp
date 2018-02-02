1463183b47946c74d804a072798452c8be4b4960
glbeaudoin <glbeaudoin@outlook.com>
2018-02-02 11:09:30 -0500

    Front page creation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var http = require('http');
+var fs = require('fs');
 
 var app = express();
 
@@ -11,10 +13,66 @@ app.use(bodyParser.urlencoded({extended:false}));
 //Set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req,res){
+app.get('/index.html', function(req,res){
     res.send('Hello World');
 });
 
 app.listen(5000, function(){
       console.log('Server Started on Port 5000...');
 })
+/*
+var http = require('http');
+var fs = require('fs');
+var formidable = require("formidable");
+var util = require('util');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Set static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+var server = http.createServer(function (req, res) {
+    if (req.method.toLowerCase() == 'get') {
+        displayForm(res);
+    } else if (req.method.toLowerCase() == 'post') {
+        processAllFieldsOfTheForm(req, res);
+    }
+
+});
+
+function displayForm(res) {
+    fs.readFile('public/index.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+                'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}
+
+function processAllFieldsOfTheForm(req, res) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+        /*Store the data from the fields in your data store.
+        The data store could be a file or database or any other store base on your application.
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.write('received the data:\n\n');
+        res.end(util.inspect({
+            answers: fields,
+            files: files
+        }));
+    });
+}
+
+server.listen(5000);
+console.log("server listening on 5000");*/
