18f1b67ad3c2a1bce616447cff9a5a96092dc51a
chrisd95 <christopher.dip@umontreal.ca>
2018-02-02 12:35:08 -0500

    ejs views

--- app.js
+++ app.js
@@ -3,8 +3,39 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var http = require('http');
 var fs = require('fs');
+var request = require('request');
 
 var app = express();
+
+var PORT = process.env.PORT || 5000
+
+// View Engine (START)
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname, 'public'));
+app.listen(PORT, () => console.log(`EJS displayed at port ${ PORT }`))
+// View Engine (END)
+
+// Global variables (START)
+app.use(function(req, res, next){
+  res.locals.errors = null;
+  next();
+});
+// Global variables (END)
+
+
+// Set Static Path (EJS,CSS,jquery,etc.) (START)
+app.use(express.static(path.join(__dirname, 'public')))
+// Set Static Path (EJS,CSS,jquery,etc.) (END)
+
+
+// Main website GET (START)
+app.get('/', function(req, res){
+    //render EJS
+    res.render('index.ejs', {
+        //render variables
+    });
+  });
+// Main website GET (END)
 
 //Body Parser Middleware
 app.use(bodyParser.json());
@@ -17,9 +48,6 @@ app.get('/index.html', function(req,res){
     res.send('Hello World');
 });
 
-app.listen(5000, function(){
-      console.log('Server Started on Port 5000...');
-})
 /*
 var http = require('http');
 var fs = require('fs');
